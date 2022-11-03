215c5d538fbcde7f013463bb21b67ce5b47487d9
Anatoliy Kolesnyk <uitpm221_kab@student.ztu.edu.ua>
2022-11-03 19:14:10 +0200

    Changed language

--- ConsoleAppDemo/Program.cs
+++ ConsoleAppDemo/Program.cs
@@ -1,8 +1,10 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello,!");
-Console.WriteLine("Hello,!");
-Console.WriteLine("Hello,!");
-Console.WriteLine("Hello,");
-Console.WriteLine("Hello,");
-Console.WriteLine("Hello,");
+Console.WriteLine("Привіт,!");
+Console.WriteLine("Привіт,!");
+Console.WriteLine("Привіт,!");
+Console.WriteLine("Привіт,!");
+Console.WriteLine("Привіт,!");
+Console.WriteLine("Привіт,!");
+
+
 
